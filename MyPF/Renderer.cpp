a6aa484fabdf3c11428b6af9e75efe36c27f75fb
Yookpo <131447294+Yookpo@users.noreply.github.com>
2026-08-19 17:29:41 +0900

    Renderer 리팩토링

--- MyPF/Renderer.cpp
+++ MyPF/Renderer.cpp
@@ -2,32 +2,45 @@
 #include "Mesh.h"
 #include "Material.h"
 #include "Texture.h"
+#include "GraphicsDevice.h"
 
 namespace My
 {
 
-	bool Renderer::Initialize(HWND mainWindow, int screenWidth, int screenHeight)
+	bool Renderer::Initialize(GraphicsDevice& graphicsDevice, int screenWidth, int screenHeight)
 	{
-		if (!InitDirect3D(mainWindow, screenWidth, screenHeight))
+		m_graphicsDevice = &graphicsDevice;
+
+		if (!m_graphicsDevice->GetDevice() || !m_graphicsDevice->GetContext()
+			|| screenWidth <= 0 || screenHeight <= 0)
 		{
 			return false;
 		}
 
-		if (!CreateRenderTargetView())
+		ID3D11Device* Device = m_graphicsDevice->GetDevice();
+
+		if (!CreateRasterizerState())
+		{
 			return false;
+		}
 
 		SetViewPort(0, 0, static_cast<float>(screenWidth), static_cast<float>(screenHeight));
 
+		if (!D3D11Utils::CreateDepthStencilState(Device, m_depthStencilState))
+		{
+			return false;
+		}
+
 		m_cameraConstantData.view = Matrix();
 		m_cameraConstantData.projection = Matrix();
-		if (!D3D11Utils::CreateConstantBuffer(m_device.Get(), m_cameraConstantData, m_cameraConstantBuffer))
+		if (!D3D11Utils::CreateConstantBuffer(Device, m_cameraConstantData, m_cameraConstantBuffer))
 		{
 			return false;
 		}
 
 		m_objectConstantData.model = Matrix();
 		m_objectConstantData.invTranspose = Matrix();
-		if (!D3D11Utils::CreateConstantBuffer(m_device.Get(), m_objectConstantData, m_objectConstantBuffer))
+		if (!D3D11Utils::CreateConstantBuffer(Device, m_objectConstantData, m_objectConstantBuffer))
 		{
 			return false;
 		}
@@ -36,22 +49,18 @@ namespace My
 		m_lightConstantData.color = Vector3(0.0f);
 		m_lightConstantData.intensity = 1.0f;
 		m_lightConstantData.pad = 0.0f;
-		if (!D3D11Utils::CreateConstantBuffer(m_device.Get(), m_lightConstantData, m_lightConstantBuffer))
+		if (!D3D11Utils::CreateConstantBuffer(Device, m_lightConstantData, m_lightConstantBuffer))
 		{
 			return false;
 		}
 
 		m_materialConstantData.baseColor = Vector3(1.0f);
 		m_materialConstantData.pad = 0.0f;
-		if (!D3D11Utils::CreateConstantBuffer(m_device.Get(), m_materialConstantData, m_materialConstantBuffer))
+		if (!D3D11Utils::CreateConstantBuffer(Device, m_materialConstantData, m_materialConstantBuffer))
 		{
 			return false;
 		}
 
-		if (!D3D11Utils::CreateDepthBuffer(m_device.Get(), screenWidth, screenHeight, m_depthStencilView, m_depthStencilState))
-		{
-			return false;
-		}
 
 		vector<D3D11_INPUT_ELEMENT_DESC> inputElements = {
 			{"POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,
@@ -65,7 +74,7 @@ namespace My
 		};
 
 		if (!D3D11Utils::CreateVertexShaderAndInputLayout(
-			m_device.Get(), L"Shaders\\simpleVertexShader.hlsl", inputElements, m_vertexShader,
+			Device, L"Shaders\\simpleVertexShader.hlsl", inputElements, m_vertexShader,
 			m_inputLayout
 		))
 		{
@@ -73,7 +82,7 @@ namespace My
 		}
 
 		if (!D3D11Utils::CreatePixelShader(
-			m_device.Get(), L"Shaders\\simplePixelShader.hlsl", m_pixelShader
+			Device, L"Shaders\\simplePixelShader.hlsl", m_pixelShader
 		))
 		{
 			return false;
@@ -89,70 +98,34 @@ namespace My
 		sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
 		sampDesc.MinLOD = 0;
 		sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
-		if (FAILED(m_device->CreateSamplerState(&sampDesc, m_samplerState.GetAddressOf())))
-		{
-			return false;
-		}
-
-		if (!CreateRasterizerState())
-		{
-			return false;
-		}
-
-
-		return true;
-	}
-
-	bool Renderer::Resize(int screenWidth, int screenHeight)
-	{
-		// 창 최소화 상태이므로 작업 없이 넘어가기
-		if (screenWidth <= 0 || screenHeight <= 0)
-		{
-			return true;
-		}
-
-		if (!m_device || !m_context || !m_swapChain)
-		{
-			OutputDebugStringW(L"No Device for resizing");
-			return false;
-		}
-
-		// 현재 RTV/DSV 연결 해제
-		m_context->OMSetRenderTargets(0, nullptr, nullptr);
-		m_renderTargetView.Reset();
-		m_depthStencilView.Reset();
-		m_depthStencilState.Reset();
-
-		HRESULT hr = m_swapChain->ResizeBuffers(0,	// 현재 개수 유지
-			screenWidth, screenHeight,	// 해상도 변경
-			DXGI_FORMAT_UNKNOWN,	// 현재 포맷 유지
-			DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH
-		);
-		if (FAILED(hr))
-		{
-			return false;
-		}
-		if (!CreateRenderTargetView())
-		{
-			return false;
-		}
-		if (!D3D11Utils::CreateDepthBuffer(m_device.Get(), screenWidth, screenHeight,
-			m_depthStencilView, m_depthStencilState))
+		if (FAILED(Device->CreateSamplerState(&sampDesc, m_samplerState.GetAddressOf())))
 		{
 			return false;
 		}
 
 		return true;
 	}
+
 
 	bool Renderer::BeginFrame(const FrameRenderData& frameRenderData, const std::array<float, 4>& m_backgroundColor)
 	{
-		m_context->ClearRenderTargetView(m_renderTargetView.Get(), m_backgroundColor.data());
-		m_context->ClearDepthStencilView(m_depthStencilView.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+		if (!m_graphicsDevice)
+		{
+			OutputDebugStringW(L"GraphicsDevice is Empty");
+			return false;
+		}
+
+		ID3D11DeviceContext* Context = m_graphicsDevice->GetContext();
+		ID3D11RenderTargetView* RTV = m_graphicsDevice->GetRTV();
+		ID3D11DepthStencilView* DSV = m_graphicsDevice->GetDSV();
+
+
+		Context->ClearRenderTargetView(RTV, m_backgroundColor.data());
+		Context->ClearDepthStencilView(DSV, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
 		// 비교: Depth Buffer를 사용하지 않는 경우
 		// m_context->OMSetRenderTargets(1, m_renderTargetView.GetAddressOf(), nullptr);
-		m_context->OMSetRenderTargets(1, m_renderTargetView.GetAddressOf(), m_depthStencilView.Get());
-		m_context->OMSetDepthStencilState(m_depthStencilState.Get(), 0);
+		Context->OMSetRenderTargets(1, &RTV, DSV);
+		Context->OMSetDepthStencilState(m_depthStencilState.Get(), 0);
 
 		// 카메라
 		m_cameraConstantData.view = frameRenderData.view;
@@ -161,7 +134,7 @@ namespace My
 		m_cameraConstantData.projection = frameRenderData.projection;
 		m_cameraConstantData.projection = m_cameraConstantData.projection.Transpose();
 
-		if (!D3D11Utils::UpdateBuffer(m_context.Get(), m_cameraConstantData, m_cameraConstantBuffer.Get()))
+		if (!D3D11Utils::UpdateBuffer(Context, m_cameraConstantData, m_cameraConstantBuffer.Get()))
 		{
 			return false;
 		}
@@ -172,18 +145,26 @@ namespace My
 		m_lightConstantData.color = frameRenderData.directionalLight.color;
 		m_lightConstantData.intensity = frameRenderData.directionalLight.intensity;
 
-		if (!D3D11Utils::UpdateBuffer(m_context.Get(), m_lightConstantData, m_lightConstantBuffer.Get()))
+		if (!D3D11Utils::UpdateBuffer(Context, m_lightConstantData, m_lightConstantBuffer.Get()))
 		{
 			return false;
 		}
 
-		m_context->PSSetConstantBuffers(0, 1, m_lightConstantBuffer.GetAddressOf());
+		Context->PSSetConstantBuffers(0, 1, m_lightConstantBuffer.GetAddressOf());
 
 		return true;
 	}
 
 	bool Renderer::DrawRenderItem(const RenderItem& renderItem)
 	{
+		if (!m_graphicsDevice)
+		{
+			OutputDebugStringW(L"GraphicsDevice is Empty");
+			return false;
+		}
+
+		ID3D11DeviceContext* Context = m_graphicsDevice->GetContext();
+
 		if (!renderItem.mesh || !renderItem.material)
 		{
 			OutputDebugStringW(L"No Data in renderItem");
@@ -209,14 +190,14 @@ namespace My
 		m_objectConstantData.invTranspose = m_objectConstantData.invTranspose.Transpose().Invert();
 
 
-		if (!D3D11Utils::UpdateBuffer(m_context.Get(), m_objectConstantData, m_objectConstantBuffer.Get()))
+		if (!D3D11Utils::UpdateBuffer(Context, m_objectConstantData, m_objectConstantBuffer.Get()))
 		{
 			return false;
 		}
 
 		// 머터리얼 변환
 		m_materialConstantData.baseColor = drawMat.GetBaseColor();
-		if (!D3D11Utils::UpdateBuffer(m_context.Get(), m_materialConstantData, m_materialConstantBuffer.Get()))
+		if (!D3D11Utils::UpdateBuffer(Context, m_materialConstantData, m_materialConstantBuffer.Get()))
 		{
 			return false;
 		}
@@ -242,27 +223,35 @@ namespace My
 			return false;
 		}
 
-		m_context->IASetInputLayout(m_inputLayout.Get());
-		m_context->IASetVertexBuffers(0, 1, &meshVertexBuffer, &stride, &offset);
-		m_context->IASetIndexBuffer(drawMesh.GetIndexBuffer(), DXGI_FORMAT_R32_UINT, 0);
-		m_context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		Context->IASetInputLayout(m_inputLayout.Get());
+		Context->IASetVertexBuffers(0, 1, &meshVertexBuffer, &stride, &offset);
+		Context->IASetIndexBuffer(drawMesh.GetIndexBuffer(), DXGI_FORMAT_R32_UINT, 0);
+		Context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
-		m_context->VSSetShader(m_vertexShader.Get(), 0, 0);
-		m_context->VSSetConstantBuffers(0, 2, constantBuffers);
-		m_context->PSSetShader(m_pixelShader.Get(), 0, 0);
+		Context->VSSetShader(m_vertexShader.Get(), 0, 0);
+		Context->VSSetConstantBuffers(0, 2, constantBuffers);
+		Context->PSSetShader(m_pixelShader.Get(), 0, 0);
 
-		m_context->PSSetShaderResources(0, 1, &albedoSRV);
-		m_context->PSSetConstantBuffers(1, 1, &pixelConstantBuffers);
-		m_context->PSSetSamplers(0, 1, m_samplerState.GetAddressOf());
+		Context->PSSetShaderResources(0, 1, &albedoSRV);
+		Context->PSSetConstantBuffers(1, 1, &pixelConstantBuffers);
+		Context->PSSetSamplers(0, 1, m_samplerState.GetAddressOf());
 
-		m_context->DrawIndexed(drawMesh.GetIndexCount(), 0, 0);
+		Context->DrawIndexed(drawMesh.GetIndexCount(), 0, 0);
 
 		return true;
 	}
 
 	bool Renderer::SetSceneViewport(float topLeftX, float topLeftY, float width, float height)
 	{
-		if (!m_context)
+		if (!m_graphicsDevice)
+		{
+			OutputDebugStringW(L"GraphicsDevice is Empty");
+			return false;
+		}
+
+		ID3D11DeviceContext* Context = m_graphicsDevice->GetContext();
+
+		if (!Context)
 		{
 			OutputDebugStringW(L"Context is Empty");
 			return false;
@@ -279,71 +268,36 @@ namespace My
 
 	bool Renderer::EndFrame()
 	{
-		if (FAILED(m_swapChain->Present(1, 0)))
+		if (!m_graphicsDevice)
 		{
-			OutputDebugStringW(L"IDXGISwapChain::Present failed");
+			OutputDebugStringW(L"GraphicsDevice is Empty");
+			return false;
+		}
+
+		if (!m_graphicsDevice->Present())
+		{
+			OutputDebugStringW(L"m_graphicsDevice::Present failed");
 			return false;
 		}
 
 		return true;
 	}
 
-
-	bool Renderer::InitDirect3D(HWND mainWindow, int screenWidth, int screenHeight)
-	{
-		// m_device, m_context 생성
-		UINT createDeviceFlags = 0;
-#if defined(DEBUG) || defined(_DEBUG)
-		createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
-#endif
-		const D3D_FEATURE_LEVEL featureLevels[2] = { D3D_FEATURE_LEVEL_11_0 };
-		D3D_FEATURE_LEVEL featureLevel;
-
-		// swapchain 생성
-		DXGI_SWAP_CHAIN_DESC sd;
-		ZeroMemory(&sd, sizeof(sd));
-
-		sd.BufferDesc.Width = screenWidth;	  // set the back buffer width
-		sd.BufferDesc.Height = screenHeight;	  // set the back buffer height
-		sd.BufferDesc.RefreshRate.Numerator = 0; // use 32-bit color
-		sd.BufferDesc.RefreshRate.Denominator = 1;
-		sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-		sd.BufferCount = 2;	 // double buffering
-
-		sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-		sd.OutputWindow = mainWindow; // the window to be used
-		sd.Windowed = TRUE;             // windowed/full-screen mode
-		sd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; // allow full-screen switching
-		sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
-
-		sd.SampleDesc.Count = 1;	// No use MSAA
-		sd.SampleDesc.Quality = 0;
-
-		if (FAILED(D3D11CreateDeviceAndSwapChain(
-			0, // Default adapter
-			D3D_DRIVER_TYPE_HARDWARE,
-			0, // No software device
-			createDeviceFlags, featureLevels, 1, D3D11_SDK_VERSION, &sd,
-			m_swapChain.GetAddressOf(), m_device.GetAddressOf(), &featureLevel,
-			m_context.GetAddressOf())))
-		{
-			OutputDebugStringW(L"D3D11CreateDeviceAndSwapChain() failed");
-			return false;
-		}
-
-		if (featureLevel != D3D_FEATURE_LEVEL_11_0)
-		{
-			OutputDebugStringW(L"D3D Feature Level 11 unsupported");
-			return false;
-		}
-
-		
-
-		return true;
-	}
 
 	void Renderer::SetViewPort(float topLeftX, float topLeftY, float screenWidth, float screenHeight)
 	{
+		if (!m_graphicsDevice)
+		{
+			OutputDebugStringW(L"GraphicsDevice is Empty");
+		}
+
+		ID3D11DeviceContext* Context = m_graphicsDevice->GetContext();
+
+		if (!Context)
+		{
+			OutputDebugStringW(L"Context is Empty");
+		}
+
 		ZeroMemory(&m_screenViewport, sizeof(D3D11_VIEWPORT));
 		m_screenViewport.TopLeftX = topLeftX;
 		m_screenViewport.TopLeftY = topLeftY;
@@ -353,40 +307,26 @@ namespace My
 		m_screenViewport.MinDepth = 0.0f;
 		m_screenViewport.MaxDepth = 1.0f;	// Note: important for depth buffering
 
-		m_context->RSSetViewports(1, &m_screenViewport);
+		Context->RSSetViewports(1, &m_screenViewport);
 	}
 
-	bool Renderer::CreateRenderTargetView()
-	{
-		ComPtr<ID3D11Texture2D> backBuffer;
-
-		if (FAILED(m_swapChain->GetBuffer(0, IID_PPV_ARGS(backBuffer.GetAddressOf()))))
-		{
-			OutputDebugStringW(L"GetBuffer() failed");
-			return false;
-		}
-
-		if (backBuffer)
-		{
-			if (FAILED(m_device->CreateRenderTargetView(
-				backBuffer.Get(), nullptr, m_renderTargetView.GetAddressOf()
-			)))
-			{
-				OutputDebugStringW(L"CreateRTV() failed");
-				return false;
-			}
-		}
-		else
-		{
-			OutputDebugStringW(L"BackBuffer is Empty");
-			return false;
-		}
-
-		return true;
-	}
 
 	bool Renderer::CreateRasterizerState()
 	{
+		if (!m_graphicsDevice)
+		{
+			OutputDebugStringW(L"GraphicsDevice is Empty");
+			return false;
+		}
+
+		ID3D11Device* Device = m_graphicsDevice->GetDevice();
+		ID3D11DeviceContext* Context = m_graphicsDevice->GetContext();
+
+		if (!Device || !Context)
+		{
+			return false;
+		}
+
 		// Create a rasterizer state
 		D3D11_RASTERIZER_DESC rastDesc;
 		ZeroMemory(&rastDesc, sizeof(D3D11_RASTERIZER_DESC)); // Need this
@@ -396,7 +336,9 @@ namespace My
 		rastDesc.FrontCounterClockwise = false;
 		rastDesc.DepthClipEnable = true; // <- zNear, zFar 확인에 필요
 
-		if (FAILED(m_device->CreateRasterizerState(&rastDesc,
+
+
+		if (FAILED(Device->CreateRasterizerState(&rastDesc,
 			m_rasterizerState.GetAddressOf())))
 		{
 			OutputDebugStringW(L"CreateRasterizerState() failed");
@@ -404,7 +346,7 @@ namespace My
 		}
 
 		// 초기화 후 해당 래스터 초기화 할 때 바로 적용
-		m_context->RSSetState(m_rasterizerState.Get());
+		Context->RSSetState(m_rasterizerState.Get());
 
 		return true;
 	}
--- MyPF/Renderer.h
+++ MyPF/Renderer.h
@@ -6,38 +6,28 @@
 
 namespace My
 {
+	class GraphicsDevice;
+
 	class Renderer
 	{
 	public:
-		bool Initialize(HWND mainWindow, int screenWidth, int screenHeight);
-		bool Resize(int screenWidth, int screenHeight);
+		bool Initialize(GraphicsDevice& graphicsDevice, int screenWidth, int screenHeight);
 
 		bool BeginFrame(const FrameRenderData&, const std::array<float, 4>&);
 		bool EndFrame();
 
 		bool DrawRenderItem(const RenderItem&);
-
 		bool SetSceneViewport(float topLeftX, float topLeftY, float width, float height);
 
-		ID3D11Device* GetDevice() const { return m_device.Get(); }
-		ComPtr<ID3D11DeviceContext> GetContext() const { return m_context; }
-
-
 	private:
-		bool InitDirect3D(HWND mainWindow, int screenWidth, int screenHeight);
-		bool CreateRenderTargetView();
 		bool CreateRasterizerState();
 		void SetViewPort(float topLeftX, float topLeftY, float screenWidth, float screenHeight);
 
 	private:
-		ComPtr<ID3D11Device> m_device;
-		ComPtr<ID3D11DeviceContext> m_context;
-		ComPtr<IDXGISwapChain> m_swapChain;	// 완성된 화면을 창에 표시
-		ComPtr<ID3D11RenderTargetView> m_renderTargetView;	// 백 버퍼를 렌더링 대상으로 연결
+		GraphicsDevice* m_graphicsDevice = nullptr;
 		ComPtr<ID3D11RasterizerState> m_rasterizerState;
 
 		// Depth buffer
-		ComPtr<ID3D11DepthStencilView> m_depthStencilView;
 		ComPtr<ID3D11DepthStencilState> m_depthStencilState;
 
 		// shader
