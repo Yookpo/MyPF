3393d067d108a01e96acb3d334b882bcbed62e26
Yookpo <131447294+Yookpo@users.noreply.github.com>
2026-08-19 18:09:27 +0900

    버퍼 핸들

---
+++ MyPF/GraphicsResourceHandle.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <cstdint>
+#include <limits>
+
+namespace My
+{
+	class BufferHandle
+	{
+	public:
+		BufferHandle()
+			: m_index{ m_invalidIndex }
+		{
+		}
+
+		explicit BufferHandle(uint32_t index)
+			: m_index(index)
+		{
+		}
+
+		uint32_t GetIndex() const { return m_index; }
+
+		bool IsValid() const
+		{
+			return m_index != m_invalidIndex;
+		}
+
+	private:
+		// 무효 인덱스 상수
+		static constexpr uint32_t m_invalidIndex = std::numeric_limits<uint32_t>::max();
+		uint32_t m_index;
+	};
+}
